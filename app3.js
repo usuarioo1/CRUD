16c5dec76bde3ae938ad87e9e5d6d59838182578
usuarioo1 <nicolasgonzalezmoller@gmail.com>
2023-08-09 23:49:54 -0400

    modificacion de hoja de estilo además de eliminación del primer html, se crea uno nuevo

--- app3.js
+++ app3.js
@@ -1,46 +1,43 @@
 // Obtener referencias a los elementos del formulario y la lista de órdenes
 const form = document.getElementById('main-form');
 const meseroInput = document.getElementById('mesero');
-const mesaInput = document.getElementById('mesa');
-const pedidoInput = document.getElementById('pedido');
-const ordenesList = document.querySelector('.orden');
+const pedidoInput = document.getElementById('orden');
+const ordenesList = document.getElementById('ordenes');
 
 // Array para almacenar las órdenes
 let ordenes = [];
 
 // Función para agregar una orden
-function agregarOrden(nombreMesero, numMesa, pedido) {
-    ordenes.push({ 
-        mesero: nombreMesero, 
-        mesa: numMesa, 
-        pedido: pedido });
+function agregarOrden(nombreMesero, pedido) {
+    ordenes.push({ mesero: nombreMesero, pedido: pedido });
     mostrarOrdenes();
 }
 
 // Función para mostrar las órdenes en la lista
 function mostrarOrdenes() {
-    ordenesList.innerHTML = '';
-    ordenes.forEach((orden, index) => {
-        const ordenElement = document.createElement('div');
-        ordenElement.classList.add('orden-item');
-        ordenElement.innerHTML = `
-            <p>Mesero: ${orden.mesero}</p>
-            <p>Mesa: ${orden.mesa}</p>
-            <p>Pedido: ${orden.pedido}</p>
-            <button class="btn-edit" data-index="${index}">Editar</button>
-            <button class="btn-delete" data-index="${index}">Eliminar</button>
-        `;
-        ordenesList.appendChild(ordenElement);
-    });
+  ordenesList.innerHTML = '';
+
+  for (let index = 0; index < ordenes.length; index++) {
+      const orden = ordenes[index];
+      const ordenElement = document.createElement('div');
+      ordenElement.classList.add('orden-item');
+      ordenElement.innerHTML = `
+          <p>Mesero: ${orden.mesero}</p>
+          <p>Pedido: ${orden.pedido}</p>
+          <button class="btn-edit" data-index="${index}">Editar</button>
+          <button class="btn-delete" data-index="${index}">Eliminar</button>
+      `;
+      ordenesList.appendChild(ordenElement);
+  }
 }
+
 
 // Función para manejar el envío del formulario
 form.addEventListener('submit', function (event) {
     event.preventDefault();
     const nombreMesero = meseroInput.value;
-    const numMesa = mesaInput.value;
     const pedido = pedidoInput.value;
-    agregarOrden(nombreMesero, numMesa, pedido);
+    agregarOrden(nombreMesero, pedido);
     form.reset();
 });
 
